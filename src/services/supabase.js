53bf1de008ac79d36f43e77d00a1087e1a5111ae
ahmedhassan4 <158503741+ahmedhassan4@users.noreply.github.com>
2024-10-26 18:18:42 +0300

    supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://sbjzwxgkhobybufulssx.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNianp3eGdraG9ieWJ1ZnVsc3N4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk4ODM1NjYsImV4cCI6MjA0NTQ1OTU2Nn0.8HH6NyUFzjD9hP9hd5fdVKNL3763WLp7ANeIHVjHY8o";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
